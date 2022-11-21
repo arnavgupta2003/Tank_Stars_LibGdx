75ecde408a67cdd8792d7d4f54a5fbd817e7ffdf
Arnav <arnav.gupta.2003@gmail.com>
2022-11-22 02:21:25 +0530

    Gaba Merge.. Done DeskLauncher

--- desktop/src/com/extinct/tankstars/DesktopLauncher.java
+++ desktop/src/com/extinct/tankstars/DesktopLauncher.java
@@ -8,6 +8,8 @@ import com.extinct.tankstars.TankStars;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1280,720);
+
 		config.setForegroundFPS(60);
 		config.setTitle("Tank Stars");
 		new Lwjgl3Application(new TankStars(), config);
